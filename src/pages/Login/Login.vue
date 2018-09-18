3e09eebca615861e574e853b2090018852e3b5b5
babyli_glb <babyli_glb@hundsun.com>
2018-09-18 10:31:26 +0800

    完成短信验证码登陆的功能

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -58,6 +58,7 @@
 
 <script>
 	import AlertTip from '../../components/AlertTip/AlertTip.vue';
+	import {reqSendCode, reqSmsLogin, rreqPwdLogin} from '../../api'
 	export default {
 		components: {
 			AlertTip,
@@ -98,47 +99,86 @@
 				// 每次指定的src要不一样
 				event.target.src = "http://localhost:4000/captcha?time=" + Date.now();
 			},
-			// 获取手机号验证码
-			getCode() {
+			// 异步获取短信验证码
+			async getCode() {
 				// 如果当前没有计时
 				if(!this.computeTime) {
 					//启动倒计时
 					this.computeTime = 30;
-					const intervalId = setInterval(() => {
+					this.intervalId = setInterval(() => {
 						this.computeTime--;
 						if(this.computeTime <=0 ) {
 							//停止计时
-							clearInterval(intervalId);
+							clearInterval(this.intervalId);
 						}
 					}, 1000);
 				}
 				// 发送Ajax请求（向指定手机号发送验证码短信）
+				const result = await reqSendCode(this.phone);
+				if(result.code ===1) {
+					// 显示提示
+					this.showAlert(result.msg);
+					// 停止计时
+					if(this.computeTime) {
+						this.computeTime = 0;
+						clearInterval(this.intervalId);
+						this.intervalId = undefined;
+					}
+				}
 			},
 			
 			// 异步登陆
-			login() {
+			async login() {
+				let result;
 				// 前台表单验证
 				if(this.loginWay) { //短信登陆
 					const {rightPhone, phone, code} = this;
 					if(!this.rightPhone) {
 						//手机号不正确
 						this.showAlert("手机号不正确");
+						return;
 					} else if(!/^\d{6}$/.test(code)) {
 						// 验证码必须是6位数字
 						this.showAlert("验证码必须是6位数字");
+						return;
 					}
+					// 发送Ajax请求短信登陆
+					result = await reqSmsLogin(phone, code);
 				} else { //密码登陆
 					const {name, pwd, captcha} = this;
 					if(!this.name) {
 						// 用户名必须指定
 						this.showAlert("用户名必须指定");
+						return;
 					} else if(!this.pwd) {
 						// 密码必须指定
 						this.showAlert("密码必须指定");
+						return;
 					} else if(!this.captcha) {
 						// 验证码必须指定
 						this.showAlert("验证码必须指定");
+						return;
 					}
+					// 发送Ajax请求密码登陆
+					result = await reqPwdLogin({name, pwd, captcha});
+				}
+				// 停止计时
+				if(this.computeTime) {
+					this.computeTime = 0;
+					clearInterval(this.intervalId);
+					this.intervalId = undefined;
+				}
+				
+				// 根据结果数据处理
+				if(result.code ===0) {
+					const user = result.data;
+					// 将user保存到vuex的state中
+					
+					// 去个人中心界面
+					this.$router.replace('/profile')
+				} else {
+					const msg = result.msg;
+					this.showAlert(msg);
 				}
 			},
 		}
